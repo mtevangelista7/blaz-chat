3db5fd0b26612a6ecde91d8b44e8055f0ec2f949
Matheus Evangelista Dos Santos <matheus.evangelista7@protonmail.com>
2024-05-24 09:08:15 -0300

    feat: small layout change online chat

--- blazchat/Program.cs
+++ blazchat/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
